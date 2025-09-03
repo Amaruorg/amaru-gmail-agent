04ec109f053728c7f6c2f7ac67f4ae7759933e83
Jaider-Rojas <185117522+Jaider-Rojas@users.noreply.github.com>
2025-09-02 22:14:34 -0500

    feat: add TailwindCSS, Prettier config, and basic UI styling

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+	  "./app/**/*.{js,ts,jsx,tsx}", // Next.js 13 app directory
+	  "./pages/**/*.{js,ts,jsx,tsx}", // pages directory if you use it
+	  "./components/**/*.{js,ts,jsx,tsx}", // any components
+	],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+  };
+  
