08e56685f84a66e57d00f70052a5921a8a306f45
Kevin Rogers <rogerscardonakevin@gmail.com>
2025-09-03 20:34:45 -0500

    feat: integrate Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,20 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+	subsets: ["latin"],
+	weight: ["400"],
+});
 
 export const metadata = {
-  title: 'Chat Gemini',
-  description: 'Gemini IA Chat',
+	title: "Chat Gemini",
+	description: "Gemini IA Chat",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${inter.className} bg-background`}>{children}</body>
+		</html>
+	);
 }
