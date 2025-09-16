6298e08de1176d63b18babfa9c733f52a49849cb
Kevin Rogers <72825328+KORC-hub@users.noreply.github.com>
2025-09-15 19:57:43 -0500

    feat(ui): add TabItem component and update exports

---
+++ src/components/ui/TabItem.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { mergeClasses } from "@/lib/tailwindUtils";
+
+const TabVariants = cva("relative text-lg transition-colors p-3 px-10", {
+	variants: {
+		variant: {
+			selected: "text-foreground hover:text-foreground cursor-default",
+			unselected: "text-muted hover:text-foreground/80 cursor-pointer active:text-foreground",
+		},
+	},
+	defaultVariants: {
+		variant: "unselected",
+	},
+});
+
+type TabVariants = VariantProps<typeof TabVariants>;
+
+type TabProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+	TabVariants & {
+		children: React.ReactNode;
+	};
+
+function TabItem({ variant, children, ...rest }: TabProps) {
+	return (
+		<button className="w-full flex items-center justify-center" {...rest}>
+			<div className={mergeClasses(TabVariants({ variant }))}>{children}</div>
+		</button>
+	);
+}
+
+export { TabItem, type TabProps };
