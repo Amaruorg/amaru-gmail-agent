3e62261080b48a690c8db59680f8e200e9cd30fd
Jaider R. <jaiderrojasd@gmail.com>
2025-09-22 06:21:33 -0500

    Revert "fix(db): update Prisma schema (#60)"
    This reverts commit cf3704e271e53fad7aaca36d0340bc6fb3c0bbc6.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,7 +12,6 @@ export const auth = betterAuth({
 		google: {
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-			scope: ["openid", "profile", "email", "https://www.googleapis.com/auth/gmail.readonly"],
 		},
 	},
 });
