9a164b859ac6a1a813be86f346fe6e802cda6fb7
Jaider R. <185117522+Jaider-Rojas@users.noreply.github.com>
2025-09-25 22:12:49 -0500

    fix: remove pricing from middleware redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,5 +14,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: ["/pricing", "/dashboard", "/settings/:path*", "/whitelist"],
+	matcher: ["/dashboard", "/settings/:path*", "/whitelist"],
 };
