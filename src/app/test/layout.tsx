302396617e457d8c2f8b6d87f810d57c2a0d767a
Jaider R. <185117522+Jaider-Rojas@users.noreply.github.com>
2025-09-25 21:53:14 -0500

    fix: correct styles import

--- src/app/test/layout.tsx
+++ src/app/test/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { Inter } from "next/font/google";
-import "@/style/globals.css";
+import "@/styles/globals.css";
 
 const inter = Inter({
 	subsets: ["latin"],
