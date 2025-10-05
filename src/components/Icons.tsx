72d56968108197996c487f1625d27459db743a80
Kevin Rogers <72825328+KORC-hub@users.noreply.github.com>
2025-10-05 00:05:25 -0500

    feat: add new icons and enhance Button and Input components with additional props

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -163,6 +163,88 @@ const CheckList = (props: iconProps) => {
 	);
 };
 
+const Collections = (props: iconProps) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			{...props}
+		>
+			<rect width="8" height="18" x="3" y="3" rx="1" />
+			<path d="M7 3v18" />
+			<path d="M20.4 18.9c.2.5-.1 1.1-.6 1.3l-1.9.7c-.5.2-1.1-.1-1.3-.6L11.1 5.1c-.2-.5.1-1.1.6-1.3l1.9-.7c.5-.2 1.1.1 1.3.6Z" />
+		</svg>
+	);
+};
+
+const Trash = (props: iconProps) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			{...props}
+		>
+			<path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+			<path d="M3 6h18" />
+			<path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+		</svg>
+	);
+};
+
+const Plus = (props: iconProps) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			{...props}
+		>
+			<path d="M5 12h14" />
+			<path d="M12 5v14" />
+		</svg>
+	);
+};
+
+const Close = (props: iconProps) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			{...props}
+		>
+			<path d="M18 6 6 18" />
+			<path d="m6 6 12 12" />
+		</svg>
+	);
+};
+
 const ArrowUp = (props: iconProps) => {
 	return (
 		<svg
@@ -280,6 +362,10 @@ export {
 	Book,
 	List,
 	CheckList,
+	Collections,
+	Trash,
+	Plus,
+	Close,
 	AmaruOutline,
 	ArrowUp,
 	ArrowDown,
